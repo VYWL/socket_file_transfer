6184d7665269c2aa12e12a4b4e41db0a7b147a9d
VYWL <dksu40@gmail.com>
2021-12-01 08:10:50 +0900

    client 리팩토링 완료

--- client/cpp/Receiver.cpp
+++ client/cpp/Receiver.cpp
@@ -16,71 +16,33 @@
 int tcp_connect(char *ip, int host);
 void errquit(char* mesg) { perror(mesg); exit(0); }
 
-std::string buf2str(char* checker, int bufflen)
-{
-    std::string tempStr; 
-
-	for(int i = 0; i < bufflen; ++i) {
-		char buf[bufflen];
-
-		snprintf(buf, bufflen, "%02x", checker[i]);
-
-		tempStr += buf;
-	}
-
-	return tempStr;
-}
+std::string buf2str(char* checker, int bufflen);
+void validationStep(int listen_sock, std::string fileNm);
+void fileDownloadStep(int listen_sock, std::string pathname);
 
 int main(int argc, char* argv[])
 {
-	int sum = 0;
-
 	int listen_sock;
-	FILE* file;
-	size_t fsize;
-	std::string fileNm = argv[3];
-	std::string errStr("00000000");
-	char buffer[MAX_SIZE];
-	char checker[4];
-	char filename[MAX_LINE], temp[5];
+	
+	std::string fileNm(argv[3]);
+	std::string pathNm(argv[4]);
 	
 	if (argc != 5) {
-		printf("사용법 : %s [server_ip] [port] [filename] [savepath]\n", argv[0]);
+		printf("사용법 : %s [server_ip] [port] [filename] [pathname]\n", argv[0]);
 		exit(0);
 	}
 
 	listen_sock = tcp_connect(argv[1], atoi(argv[2]));
 	
-	if (listen_sock == -1)
-		errquit("tcp_connect fail");
+	if (listen_sock == -1) errquit("tcp_connect fail");
 
+	validationStep(listen_sock, fileNm);
 
-	// STEP 1 :: send filename to node server (filename size, max 511)
-	send(listen_sock, fileNm.c_str(), fileNm.size(), 0);
-
-	// STEP 2 :: receive check Result as Buffer from server (4 bytes);
-	int nbyte = recv(listen_sock, checker, 4, 0);
-	
-	// STEP 3 :: Using buf2str function, get checkerString
-	std::string checkerString = buf2str(checker, CHECK_BUF_SIZE);
-	
-	// STEP 4 :: Compare checkerString with errorString.
-	// If same, this process will be shut down.
-	if(checkerString == errStr) return std::cout << "[ERROR] No File Exists.\n", 0;
-
-	// If file exists, start downloading file.
-	// First, get File pointer with "wb" option and pathname(argv[4])
-	file = fopen(argv[4], "wb");
-
-	nbyte = MAX_SIZE;
-
-	while (nbyte) {
-		nbyte = recv(listen_sock, buffer, MAX_SIZE, 0);
-		fwrite(buffer, sizeof(char), nbyte, file);
-	}
+	fileDownloadStep(listen_sock, pathNm);
 
 	std::cout << "[SUCCESS] \"" << fileNm << "\" Download Finished.\n";
-	fclose(file);
+
+	return 0;
 }
 
 int tcp_connect(char* ip, int port) {
@@ -101,4 +63,61 @@ int tcp_connect(char* ip, int port) {
 	}
 
 	return sd;
+}
+
+void validationStep(int listen_sock, std::string fileNm)
+{
+	std::string errStr("00000000");
+	char checker[4];
+
+	// STEP 1 :: send filename to node server (filename size, max 511)
+	send(listen_sock, fileNm.c_str(), fileNm.size(), 0);
+
+	// STEP 2 :: receive check Result as Buffer from server (4 bytes);
+	int nbyte = recv(listen_sock, checker, 4, 0);
+	
+	// STEP 3 :: Using buf2str function, get checkerString
+	std::string checkerString = buf2str(checker, CHECK_BUF_SIZE);
+	
+	// STEP 4 :: Compare checkerString with errorString.
+	// If same, this process will be shut down.
+	if(checkerString == errStr) 
+	{
+		std::cout << "[ERROR] No File Exists.\n";
+		exit(-1);
+	}
+}
+
+void fileDownloadStep(int listen_sock, std::string pathname)
+{
+	FILE* file;
+	char buffer[MAX_SIZE];
+
+	// If file exists, start downloading file.
+	// First, get File pointer with "wb" option and pathname(argv[4])
+	file = fopen(pathname.c_str(), "wb");
+
+	int nbyte = MAX_SIZE;
+
+	while (nbyte) {
+		nbyte = recv(listen_sock, buffer, MAX_SIZE, 0);
+		fwrite(buffer, sizeof(char), nbyte, file);
+	}
+
+	fclose(file);
+}
+
+std::string buf2str(char* checker, int bufflen)
+{
+    std::string tempStr; 
+
+	for(int i = 0; i < bufflen; ++i) {
+		char buf[bufflen];
+
+		snprintf(buf, bufflen, "%02x", checker[i]);
+
+		tempStr += buf;
+	}
+
+	return tempStr;
 }
--- client/cpp/init.sh
+++ client/cpp/init.sh
@@ -1,3 +1,3 @@
 # rm -rf hi*.pdf
 
-./Receiver 127.0.0.1 2031 test ./hi3.pdf
+./Receiver 127.0.0.1 2031 test2 ./hi6.pdf
